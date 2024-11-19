6a225820487a654c4bde6876d437cbc187a6c2c9
coder7475 <robiulhossain7475@gmail.com>
2024-11-19 19:38:36 +0600

    feat: load the model

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ import uvicorn
 app = FastAPI(title="Iris Classifier API", 
               description="API for classifying Iris flower", 
               version="1.0.0")
+
+model = joblib.load('iris_model.joblib')
